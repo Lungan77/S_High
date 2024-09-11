caf3fa4530a4cbd619590cc8fcc6c0856924906b
Lungan77 <lunganintuthuko1@gmail.com>
2024-09-11 19:57:45 +0200

    add_about

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero_section from "@/components/hero_section";
+import About_us from "@/components/about";
 
 export default function Home() {
   return (
     <>
-    <Hero_section/>
+      <Hero_section />
+      <About_us />
     </>
   );
 }
--- components/hero_section.jsx
+++ components/hero_section.jsx
@@ -6,7 +6,7 @@ function Hero_section() {
             <div className='hero_section_center'>
                 <h1>Empowering Future Leaders</h1>
                 <p>At S-High, we nurture young minds with a passion for learning and a commitment to excellence.</p>
-                <a href="/apply" class="cta-button">Apply Now</a>
+                <a href="/apply" className="cta-button">Apply Now</a>
             </div>
         </section>
     )
