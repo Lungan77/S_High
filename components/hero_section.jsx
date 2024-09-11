0738ab01c86557cfc60b76894abef38407487cdd
Lungani <lunganintuthuko1@gmail.com>
2024-09-11 16:11:59 +0200

    initial commit

---
+++ components/hero_section.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Hero_section() {
+    return (
+        <section id='hero_section'>
+            <div className='hero_section_center'>
+                <h1>Empowering Future Leaders</h1>
+                <p>At S-High, we nurture young minds with a passion for learning and a commitment to excellence.</p>
+                <a href="/apply" class="cta-button">Apply Now</a>
+            </div>
+        </section>
+    )
+}
+
+export default Hero_section
---
+++ components/nav.jsx
@@ -0,0 +1,57 @@
+'use client'
+
+import Link from "next/link"
+import { useState } from "react";
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+const iconButtonStyle = {
+  background: 'transparent',
+  border: 'none',
+  cursor: 'pointer',
+  padding: '10px',
+};
+
+
+function Nav() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
+  return (
+    <div className='header'>
+      <div className="logo">
+        <h1>S-High</h1>
+      </div>
+
+      <nav className="nav_display">
+        <ul>
+          <li><Link href='#'>Home</Link><span>|</span></li>
+          <li><Link href='#'>About</Link><span>|</span></li>
+          <li><Link href='#'>Blogs</Link><span>|</span></li>
+          <li><Link href='#'>Contacts</Link><span>|</span></li>
+        </ul>
+      </nav>
+
+      <button onClick={toggleNav} style={iconButtonStyle} className="icon_display">
+        {isNavOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+      </button>
+
+      {
+        isNavOpen && (
+          <nav className="s_nav">
+            <ul>
+              <li><Link href='#'>Home</Link></li>
+              <li><Link href='#'>About</Link></li>
+              <li><Link href='#'>Blogs</Link></li>
+              <li><Link href='#'>Contacts</Link></li>
+            </ul>
+          </nav>
+        )
+      }
+    </div>
+  )
+}
+
+export default Nav
