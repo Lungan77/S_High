7d2c982373c6c5e833d78c9473ad6611bbf0bba3
Lungani <lunganintuthuko1@gmail.com>
2024-09-12 13:00:49 +0200

    add_logo

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Link from "next/link"
+import Image from "next/image";
 import { useState } from "react";
 import { FaBars, FaTimes } from 'react-icons/fa';
 
@@ -20,36 +21,45 @@ function Nav() {
   };
 
   return (
-    <div className='header bg-gray-900 text-white'>
-      <div className="logo">
-        <h1>S-High</h1>
+    <div className="header bg-gray-900 text-white ">
+      <div className='container head_b'>
+        <div className="logo">
+          <Link href='#'>
+            <Image
+              src='https://cdn-icons-png.flaticon.com/128/3074/3074058.png'
+              width={40}
+              height={40}
+              className="bg-gray-300"
+            />
+          </Link>
+        </div>
+
+        <nav className="nav_display">
+          <ul>
+            <li><Link href='#'>Home</Link><span>|</span></li>
+            <li><Link href='#'>About</Link><span>|</span></li>
+            <li><Link href='#'>Blogs</Link><span>|</span></li>
+            <li><Link href='#'>Contacts</Link><span>|</span></li>
+          </ul>
+        </nav>
+
+        <button onClick={toggleNav} style={iconButtonStyle} className="icon_display">
+          {isNavOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        </button>
+
+        {
+          isNavOpen && (
+            <nav className="s_nav">
+              <ul>
+                <li><Link href='#'>Home</Link></li>
+                <li><Link href='#'>About</Link></li>
+                <li><Link href='#'>Blogs</Link></li>
+                <li><Link href='#'>Contacts</Link></li>
+              </ul>
+            </nav>
+          )
+        }
       </div>
-
-      <nav className="nav_display">
-        <ul>
-          <li><Link href='#'>Home</Link><span>|</span></li>
-          <li><Link href='#'>About</Link><span>|</span></li>
-          <li><Link href='#'>Blogs</Link><span>|</span></li>
-          <li><Link href='#'>Contacts</Link><span>|</span></li>
-        </ul>
-      </nav>
-
-      <button onClick={toggleNav} style={iconButtonStyle} className="icon_display">
-        {isNavOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
-      </button>
-
-      {
-        isNavOpen && (
-          <nav className="s_nav">
-            <ul>
-              <li><Link href='#'>Home</Link></li>
-              <li><Link href='#'>About</Link></li>
-              <li><Link href='#'>Blogs</Link></li>
-              <li><Link href='#'>Contacts</Link></li>
-            </ul>
-          </nav>
-        )
-      }
     </div>
   )
 }
